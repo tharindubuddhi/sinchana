7581ac26f9893d06a6f45ddb58ef6c284d19720e
tharindu.java@gmail.com <tharindu.java@gmail.com@2cb8ef29-2ba7-ed43-7f15-89a775e070a4>
2012-08-17 15:37:03 +0000

    licensing template added

--- Sinchana/src/sinchana/SinchanaServer.java
+++ Sinchana/src/sinchana/SinchanaServer.java
@@ -1,7 +1,37 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/************************************************************************************
+     
+ * Sinchana Distributed Hash table 
+ 
+ * Copyright (C) 2012 Sinchana DHT - Department of Computer Science &               
+ * Engineering, University of Moratuwa, Sri Lanka. Permission is hereby 
+ * granted, free of charge, to any person obtaining a copy of this 
+ * software and associated documentation files of Sinchana DHT, to deal 
+ * in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation 
+ * and/or other materials provided with the distribution.
+
+ * Neither the name of University of Moratuwa, Department of Computer Science 
+ * & Engineering nor the names of its contributors may be used to endorse or 
+ * promote products derived from this software without specific prior written 
+ * permission.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * SOFTWARE.                                                                    
+ ************************************************************************************/
+
 package sinchana;
 
 import sinchana.dataStore.SinchanaDataStoreInterface;
@@ -16,6 +46,7 @@ import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import sinchana.chord.ChordTable;
 import sinchana.connection.ConnectionPool;
+import sinchana.pastry.PastryTable;
 import sinchana.tapastry.TapestryTable;
 import sinchana.thrift.Message;
 import sinchana.thrift.MessageType;
@@ -32,7 +63,9 @@ public class SinchanaServer extends Node {
 
 	public static final BigInteger GRID_SIZE = new BigInteger("2", 16).pow(160);
 	private final PortHandler portHandler = new IOHandler(this);
-	private final RoutingHandler routingHandler = new TapestryTable(this);
+//	private final RoutingHandler routingHandler = new ChordTable(this);
+//    private final RoutingHandler routingHandler = new TapestryTable(this);
+    private final RoutingHandler routingHandler = new PastryTable(this);
 	private final MessageHandler messageHandler = new MessageHandler(this);
 	private final ConnectionPool connectionPool = new ConnectionPool(this);
 	private final ClientHandler clientHandler = new ClientHandler(this);
@@ -229,6 +262,7 @@ public class SinchanaServer extends Node {
 		this.getMessageHandler().queueMessage(message, MessageQueue.PRIORITY_LOW);
 	}
 
+    
 	public byte[] request(byte[] destination, byte[] message) {
 		return this.clientHandler.addRequest(destination, message, MessageType.REQUEST, null, true).data;
 	}
